a5e8a9e6ef420b1beae400b8a84d127adcabe77e
sorinteican <sorincatalinteican@gmail.com>
2016-09-04 13:08:30 +0300

    Initial project

---
+++ src/main/java/com/test/ci/App.java
@@ -0,0 +1,13 @@
+package com.test.ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
